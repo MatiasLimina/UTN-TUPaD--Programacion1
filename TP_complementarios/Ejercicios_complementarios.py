24d92a6975193cedb65257a0726a47f84bc8866b
Matias <matiaslimina@gmail.com>
2025-08-21 12:41:16 -0300

    Create Ejercicios_complementarios.py

---
+++ TP_complementarios/Ejercicios_complementarios.py
@@ -0,0 +1,25 @@
+#Ejercicio 1
+numero1 = 25
+print (f"La variable numero1: {numero1}")
+
+#Ejercicio 2
+numero2 = 30.5
+print(f"La variable numero2: {numero2}")
+
+#Ejercicio 3
+suma = numero1+numero2
+print(f"Suma de numero1 y numero2: {suma}")
+
+#Ejercicio 4
+resta = numero1-numero2
+multiplicacion = numero1*numero2
+division= numero1/numero2
+print(f"Resta de numero1 y numero2: {resta}")
+print(f"Multiplicacion de numero1 y numero2: {multiplicacion}")
+print(f"Division de numero 1 y numero2: {division}")
+
+#Ejercicio 5
+nombre="Matias"
+print(f"Mi nombre es {nombre}")
+
+#Ejercicio 6
