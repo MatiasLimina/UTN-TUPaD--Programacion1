8f0b85889635d47c6785c0257bb0d383b1b71acc
Matias <matiaslimina@gmail.com>
2025-08-21 15:51:38 -0300

    Creación variable propina_15
    Variable que calcula una propina del 15%

--- Ejercicio_secuenciales/Ejercicio_secuenciales.py
+++ Ejercicio_secuenciales/Ejercicio_secuenciales.py
@@ -1,4 +1,4 @@
 #Calculadora de propinas de un restaurante
 total_cuenta = float(input("Ingrese el total de la cuenta a pagar "))
 propina_10 = total_cuenta*0.10
-print(propina_10)
+propina_15 = total_cuenta*0.15
