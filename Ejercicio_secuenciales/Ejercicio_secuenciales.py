9f2d404721d9bd5d9ea244239edd64686895b803
Matias <matiaslimina@gmail.com>
2025-08-21 15:56:55 -0300

    Creación total_pagar_15
    Variable que calcula el total a pagar sumandole una propina del 15%

--- Ejercicio_secuenciales/Ejercicio_secuenciales.py
+++ Ejercicio_secuenciales/Ejercicio_secuenciales.py
@@ -2,5 +2,7 @@
 total_cuenta = float(input("Ingrese el total de la cuenta a pagar "))
 propina_10 = total_cuenta*0.10
 propina_15 = total_cuenta*0.15
-total_pagar_10= total_cuenta + propina_10
+total_pagar_10 = total_cuenta + propina_10
 print(f"El total de su cuenta es de ${total_cuenta}, sumandole una propina del 10%, su cuenta final queda en ${total_pagar_10}")
+total_pagar_15 = total_cuenta + propina_15
+print(f"El total de su cuenta es de ${total_cuenta}, sumandole una propina del 15%, su cuenta final queda en ${total_pagar_15}")
