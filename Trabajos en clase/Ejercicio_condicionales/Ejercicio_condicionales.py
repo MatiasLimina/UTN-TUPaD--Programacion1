f456dee57e34bf66da8e554a396927acdfaae122
Matias <matiaslimina@gmail.com>
2025-08-25 11:44:19 -0300

    Validar tipo de datos variables

--- Trabajos en clase/Ejercicio_condicionales/Ejercicio_condicionales.py
+++ Trabajos en clase/Ejercicio_condicionales/Ejercicio_condicionales.py
@@ -3,5 +3,33 @@ print(fecha)
 
 dia = fecha.split(",")
 dia = dia[0]
-DD = fecha.split("/")
+dia = dia.lower()
+print(dia)
+
+DD = fecha.split(",")
+DD = DD[1]
+DD = (DD)
 print(DD)
+
+DD = DD.split("/")
+MM = DD[1]
+MM = (MM)
+
+DD = (DD[0])
+DD = DD.strip()
+print (DD,MM)
+
+if dia.isalpha(): 
+    print ("Es valido")
+else:
+    print("Invalido")
+
+if DD.isnumeric():
+    print("Es numero")
+else:
+    print("No es num")
+
+if MM.isnumeric():
+    print("Es numero")
+else:
+    print("No es num")
