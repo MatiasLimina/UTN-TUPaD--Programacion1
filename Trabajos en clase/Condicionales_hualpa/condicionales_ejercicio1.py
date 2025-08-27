8a5998b8767a6f5158216b2e5aa49255887b106a
Matias <matiaslimina@gmail.com>
2025-08-27 09:57:11 -0300

    Correccion errores

--- Trabajos en clase/Condicionales_hualpa/condicionales_ejercicio1.py
+++ Trabajos en clase/Condicionales_hualpa/condicionales_ejercicio1.py
@@ -9,20 +9,32 @@ else:
     print("Ingrese un valor valido")
 
 promedio = input("Ingrese su promedio general")
-
-if promedio.isdigit():
-    promedio=float(promedio)
-    if promedio>= 0 and promedio<=10:
-        print("Usted ingreso un promedio valido")
-    else:
-        print("Ingrese un valor valido")
+print(type(promedio))
+promedio=float(promedio)
+print(type(promedio))
+if promedio>= 0 and promedio<=10:
+    print("Usted ingreso un promedio valido")
 else:
     print("Ingrese un valor valido")
 
 ingreso_familiar = input("Indique el ingreso total de su grupo familiar")
 if ingreso_familiar.isdigit() :
+    ingreso_familiar=int(ingreso_familiar)
     print("Numero valido")
 else:
     print("Ingrese un valor valido")
+
+print(f"{nombre_apellido}, edad: {edad}, con un promedio de {promedio} y un ingreso total")
+#Promedio
+
+if promedio<6:
+    print("RECHAZADO")
+else:
+    if ingreso_familiar < 300000:
+        print("Usted tiene acceso a la beca completa")
+    elif ingreso_familiar>=300000 and ingreso_familiar<=600000:
+        print("Usted tiene acceso a una media beca")
+    elif ingreso_familiar>600000:
+        print("RECHAZADO")
 
 
