34ccf2f9e45cb133519810386bbf8184833166e8
Matias <matiaslimina@gmail.com>
2025-08-27 09:19:15 -0300

    crear variables nombre y edad

--- Trabajos en clase/Condicionales_hualpa/condicionales_ejercicio1.py
+++ Trabajos en clase/Condicionales_hualpa/condicionales_ejercicio1.py
@@ -1,1 +1,5 @@
 #Ejercicio 1 becas estudiantiles
+nombre = input("Ingrese su nombre")
+apellido = input("Ingrese su apellido")
+nombre_apellido = nombre +" "+ apellido
+edad = int(input("Ingrese su edad"))
