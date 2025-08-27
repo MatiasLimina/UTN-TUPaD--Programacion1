c691e3166de8788d06f1f97f876d6c4b4d82a6d8
Matias <matiaslimina@gmail.com>
2025-08-27 09:29:23 -0300

    crear promedio validacion

--- Trabajos en clase/Condicionales_hualpa/condicionales_ejercicio1.py
+++ Trabajos en clase/Condicionales_hualpa/condicionales_ejercicio1.py
@@ -1,10 +1,21 @@
 #Ejercicio 1 becas estudiantiles
-nombre = input("Ingrese su nombre")
-apellido = input("Ingrese su apellido")
+nombre = input("Ingrese su nombre ")
+apellido = input("Ingrese su apellido ")
 nombre_apellido = nombre +" "+ apellido
-edad = input("Ingrese su edad")
+edad = input("Ingrese su edad ")
 if edad.isdigit() :
     print("Edad valida")
 else:
     print("Ingrese un valor valido")
 
+promedio = input("Ingrese su promedio general")
+
+if promedio.isdigit():
+    promedio=float(promedio)
+    if promedio>= 0 and promedio<=10:
+        print("Usted ingreso un promedio valido")
+    else:
+        print("Ingrese un valor valido")
+
+else:
+    print("Ingrese un valor valido")
