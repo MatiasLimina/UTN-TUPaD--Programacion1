b166a9ed0a501d19800429bb9ee53ca3821bb873
Matias <matiaslimina@gmail.com>
2025-08-28 17:47:26 -0300

    Resolución ejercicio 4

--- Unidad 2/TP_condicionales.py
+++ Unidad 2/TP_condicionales.py
@@ -16,3 +16,14 @@ if numero%2 == 0:
     print("Ha ingresado un número par")
 else:
     print("Por favor, ingrese un número par")
+
+#Ejercicio 4
+edad_2 = int(input("Ingrese su edad: "))
+if edad_2<12:
+    print("Niño/a")
+elif edad_2 >=12 and edad_2<=18:
+    print("Adolescente")
+elif edad_2 >=18 and edad_2<=30:
+    print("Adulto/a joven")
+else:
+    print("Adulto/a")
