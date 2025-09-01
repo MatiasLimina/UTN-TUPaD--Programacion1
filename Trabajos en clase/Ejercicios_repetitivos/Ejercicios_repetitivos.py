758671b5dece1f3e2e5f85fc36d0d1824c8574b9
Matias <matiaslimina@gmail.com>
2025-09-01 11:15:46 -0300

    tp repetitivos creacion variables

---
+++ Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
@@ -0,0 +1,9 @@
+abecedario = "abcdefghijklmnñopqrstuvwxyz"
+mensaje_1 = input("Ingrese el mensaje para el oficial 1")
+mensaje_2 = input("Ingrese el mensaje para el oficial 2")
+mensaje_3 = input("Ingrese el mensaje para el oficial 3")
+mensaje_4 = input("Ingrese el mensaje para el oficial 4")
+mensaje_5 = input("Ingrese el mensaje para el oficial 5")
+
+
+
