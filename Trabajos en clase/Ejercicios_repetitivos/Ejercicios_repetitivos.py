74cae9a747fc91560d55e5d62b2a4c9d79fbd4ca
Matias <matiaslimina@gmail.com>
2025-09-01 11:34:05 -0300

    Recorrido al mensaje

--- Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
+++ Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
@@ -6,5 +6,9 @@ mensajes = 5
 for i in range(0,mensajes):
     mensaje_oficial = input(f"Ingrese el mensaje a encriptar para el oficial {i+1}: ")
     print (mensaje_oficial)
+    long_mensaje =int(len(mensaje_oficial))
+    for j in range(0,long_mensaje):
+        print(mensaje_oficial[j:j+1])
+        
 
 
