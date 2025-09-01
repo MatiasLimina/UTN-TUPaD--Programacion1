61ecc43b5c2c7a638735b8dbf098baaa78f9bade
Matias <matiaslimina@gmail.com>
2025-09-01 11:19:13 -0300

    Crear iteracion mensajes

--- Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
+++ Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
@@ -5,5 +5,8 @@ mensaje_3 = input("Ingrese el mensaje para el oficial 3")
 mensaje_4 = input("Ingrese el mensaje para el oficial 4")
 mensaje_5 = input("Ingrese el mensaje para el oficial 5")
 
-
+mensajes = 5
+for mensajes in (0,5):
+    encriptado = int(input("De cuantos lugares desea correr las letras"))
+    
 
