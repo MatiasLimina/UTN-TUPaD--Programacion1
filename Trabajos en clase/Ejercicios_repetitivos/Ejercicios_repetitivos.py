a126ec72cae7dd12885e8f9e4cf11e3660018299
Matias <matiaslimina@gmail.com>
2025-09-01 11:24:50 -0300

    Arreglo ingreso de mensajes

--- Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
+++ Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
@@ -1,12 +1,10 @@
 abecedario = "abcdefghijklmnñopqrstuvwxyz"
-mensaje_1 = input("Ingrese el mensaje para el oficial 1")
-mensaje_2 = input("Ingrese el mensaje para el oficial 2")
-mensaje_3 = input("Ingrese el mensaje para el oficial 3")
-mensaje_4 = input("Ingrese el mensaje para el oficial 4")
-mensaje_5 = input("Ingrese el mensaje para el oficial 5")
+encriptado = int(input("De cuantos lugares desea correr las letras"))
+
 
 mensajes = 5
-for mensajes in (0,5):
-    encriptado = int(input("De cuantos lugares desea correr las letras"))
-    
+for i in range(0,mensajes):
+    mensaje_oficial = input(f"Ingrese el mensaje a encriptar para el oficial {i+1}: ")
+    print (mensaje_oficial)
+
 
