b867a8920976b55582005aff697c88588da7a58d
Matias <matiaslimina@gmail.com>
2025-09-01 11:40:08 -0300

    Correcion interacion mensaje

--- Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
+++ Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
@@ -4,11 +4,11 @@ encriptado = int(input("De cuantos lugares desea correr las letras"))
 
 mensajes = 5
 for i in range(0,mensajes):
-    mensaje_oficial = input(f"Ingrese el mensaje a encriptar para el oficial {i+1}: ")
+    mensaje_oficial = input(f"Ingrese el mensaje a encriptar para el oficial {i+1}: ").lower()
     print (mensaje_oficial)
-    long_mensaje =int(len(mensaje_oficial))
-    for j in range(0,long_mensaje):
-        print(mensaje_oficial[j:j+1])
-        
+    
+    for l in mensaje_oficial:
+
+    
 
 
