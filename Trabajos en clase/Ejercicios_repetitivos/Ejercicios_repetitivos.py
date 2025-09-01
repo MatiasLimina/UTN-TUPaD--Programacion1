e03a19d15d1e0522209f4fc1552efeb90911c80b
Matias <matiaslimina@gmail.com>
2025-09-01 11:47:31 -0300

    Encriptado terminado

--- Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
+++ Trabajos en clase/Ejercicios_repetitivos/Ejercicios_repetitivos.py
@@ -5,10 +5,16 @@ encriptado = int(input("De cuantos lugares desea correr las letras"))
 mensajes = 5
 for i in range(0,mensajes):
     mensaje_oficial = input(f"Ingrese el mensaje a encriptar para el oficial {i+1}: ").lower()
-    print (mensaje_oficial)
-    
+    mensaje_encrip = ""
     for l in mensaje_oficial:
+        if l in abecedario:
+            posicion = abecedario.index(l)
+            nueva_letra  = abecedario[(posicion+encriptado)%27]
+            mensaje_encrip += nueva_letra
+        else:
+            mensaje_encrip += l
+    print(mensaje_encrip)
 
-    
+
 
 
