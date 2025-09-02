0c6aa2caa41f1b56d8b31fbb098cab9692d3eb96
Matias <matiaslimina@gmail.com>
2025-09-02 19:39:38 -0300

    Mostrar info cliente

--- Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio3.py
+++ Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio3.py
@@ -5,3 +5,10 @@ cuit = input("Ingrese su CUIT")
 if cuit[2] != "-" or cuit[11] != "-":
     print ("Ingrese un CUIT válido")
 
+ingresos = float(input("Indique sus ingresos mensuales "))
+anti_laboral = int(input("Ingrese la cantidad de años correspondiente a su antiguedad laboral "))
+hist_crediticio = input("Situacion crediticia:  bueno regular malo ").lower()
+
+print(f"Cliente: {nombre.title()} \n CUIT: {cuit} \n Ingresos: {ingresos} \n Antiguedad: {anti_laboral} \n Historial crediticio: {hist_crediticio}" )
+
+
