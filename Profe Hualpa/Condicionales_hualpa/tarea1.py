3effb6031143712695a8ad346c7db797d711403b
Matias <matiaslimina@gmail.com>
2025-09-02 20:37:45 -0300

    Resolucion tarea ejercicio 1

--- Profe Hualpa/Condicionales_hualpa/tarea1.py
+++ Profe Hualpa/Condicionales_hualpa/tarea1.py
@@ -1,6 +1,23 @@
 #Clasificacion de impuestos
 nombre = input("Ingrese su nombre completo ")
-edad = input("Ingrese su edad ")
-ingresos_anual = input("Indique sus ingresos anuales ")
+edad =int(input("Ingrese su edad ")) 
+ingresos_anual =float(input("Indique sus ingresos anuales ")) 
+impuesto_final = 0
+print(f"Cliente: {nombre.title()} \n Edad: {edad} \n Ingresos anuales: ${ingresos_anual} ")
 
-print(f"Cliente: {nombre.title()} \n Edad: {anti_laboral} \n Ingresos anuales: ${ingresos_anual} ")
+if ingresos_anual < 500000:
+    print("Usted no debe pagar impuestos")
+elif ingresos_anual >= 500000 and ingresos_anual < 2000000 :
+    impuesto_final = ingresos_anual * 0.10
+    print(f"Usted debe pagar un 10% de impuestos, el total a pagar sera de ${impuesto_final}")
+elif ingresos_anual >= 2000000 and ingresos_anual < 5000000 :
+    impuesto_final = ingresos_anual * 0.20
+    print(f"Usted debe pagar un 20% de impuestos, el total a pagar sera de ${impuesto_final}")
+elif ingresos_anual >= 5000000 :
+    impuesto_final = ingresos_anual * 0.35
+    print(f"Usted debe pagar un 35% de impuestos, el total a pagar sera de ${impuesto_final}")
+
+if edad > 65 :
+    impuesto_final = impuesto_final / 2
+    print (f"Por su edad, se le aplica una reducción del 50%, su total a pagar final es de ${impuesto_final}")
+
