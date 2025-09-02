87300781caff18fdeb568dae6a30f9e1840dbfe1
Matias <matiaslimina@gmail.com>
2025-09-02 19:23:07 -0300

    Creación sist de gestión

--- Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio2.py
+++ Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio2.py
@@ -9,4 +9,16 @@ edad = int(input("Ingrese su edad "))
 obra_social = input("Tiene obra social?  SI / NO").upper()
 prioridad = int(input("Prioridad médica: 1) Emergencia  2) Urgente  3) Control "))
 
+if prioridad == 1 :
+    print("Usted a sido asignado a la guardia")
+elif prioridad == 2 :
+    if obra_social == "SI" :
+        print("Se le asignara un turno para dentro de menos de 24h")
+    else:
+        print("Se le asignara un turno para dentro de 48h")
+else:
+    if edad > 65 :
+        print ("Usted recibira un turno dentro de las proximas 72h")
+    else:
+        print ("Usted recibira un turno dentro de los proximos 7 dias")
 
