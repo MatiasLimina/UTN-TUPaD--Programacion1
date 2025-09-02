f7806d820bcfeb1dd66dc5dc2d056f0eb3036b9f
Matias <matiaslimina@gmail.com>
2025-09-02 20:19:47 -0300

    Mostrar info cliente

--- Profe Hualpa/Condicionales_hualpa/tarea1.py
+++ Profe Hualpa/Condicionales_hualpa/tarea1.py
@@ -1,1 +1,6 @@
 #Clasificacion de impuestos
+nombre = input("Ingrese su nombre completo ")
+edad = input("Ingrese su edad ")
+ingresos_anual = input("Indique sus ingresos anuales ")
+
+print(f"Cliente: {nombre.title()} \n Edad: {anti_laboral} \n Ingresos anuales: ${ingresos_anual} ")
