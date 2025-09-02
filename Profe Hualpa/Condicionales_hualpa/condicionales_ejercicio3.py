cad39d367b96f32032f356615dc725d535c7096b
Matias <matiaslimina@gmail.com>
2025-09-02 19:45:07 -0300

    Crear sist de evaluacion de credito

--- Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio3.py
+++ Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio3.py
@@ -9,6 +9,17 @@ ingresos = float(input("Indique sus ingresos mensuales "))
 anti_laboral = int(input("Ingrese la cantidad de años correspondiente a su antiguedad laboral "))
 hist_crediticio = input("Situacion crediticia:  bueno regular malo ").lower()
 
-print(f"Cliente: {nombre.title()} \n CUIT: {cuit} \n Ingresos: {ingresos} \n Antiguedad: {anti_laboral} \n Historial crediticio: {hist_crediticio}" )
+print(f"Cliente: {nombre.title()} \n CUIT: {cuit} \n Ingresos: ${ingresos} \n Antiguedad: {anti_laboral} \n Historial crediticio: {hist_crediticio}" )
 
+if hist_crediticio == "malo":
+    print ("Rechazado")
+elif ingresos < 200000:
+    print("Rechazado")
+elif ingresos >= 200000 and anti_laboral < 2:
+    print ("Usted puede pedir un maximo de $500.000")
+elif ingresos >= 200000 and anti_laboral >= 2:
+    if hist_crediticio == "regular":
+        print("Puede pedir hasta $1.000.000")
+    elif hist_crediticio == "bueno":
+        print("Puede pedir hasta $3.000.000")
 
