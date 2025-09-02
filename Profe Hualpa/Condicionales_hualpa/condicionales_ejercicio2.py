04c9d6055146634666923a0d33c1531529a0bba3
Matias <matiaslimina@gmail.com>
2025-09-02 19:19:01 -0300

    Crear var faltantes

--- Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio2.py
+++ Profe Hualpa/Condicionales_hualpa/condicionales_ejercicio2.py
@@ -1,7 +1,12 @@
 #Gestión de turnos hospitalarios
-nombre = input("Ingrese su nombre completo").title()
-dni = input("Ingrese su DNI sin puntos")
+nombre = input("Ingrese su nombre completo ").title()
+dni = input("Ingrese su DNI sin puntos ")
 len_dni = len(dni)
 if len_dni != 11 :
     print ("Usted no ingreso un DNI válido")
+
+edad = int(input("Ingrese su edad "))
+obra_social = input("Tiene obra social?  SI / NO").upper()
+prioridad = int(input("Prioridad médica: 1) Emergencia  2) Urgente  3) Control "))
+
 
