4760c3851721bc8756a38f4ddca769f7be68077c
Matias <matiaslimina@gmail.com>
2025-09-03 09:30:42 -0300

    validacion patente

--- Profe Hualpa/Repetitivos/Ejercicio_repetitivo.py
+++ Profe Hualpa/Repetitivos/Ejercicio_repetitivo.py
@@ -1,1 +1,15 @@
-patente = input("Ingrese su patente ").upper()
+
+abc = "abcdefghijklmnopqrstuvwxyz"
+patente_valida = False
+while patente_valida == False:
+    patente = input("Ingrese su patente ").upper()
+    print(len(patente))
+    if len(patente) == 6:
+        letras_patentes = patente[0:2]
+        num_patentes =patente[3:5]
+
+        if letras_patentes.isalpha() and num_patentes.isdigit():
+            patente_valida = True
+    else:
+        print("Ingrese una patente válida")
+print("Usted ingreso una patente válida")
