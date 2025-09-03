b4e95e824ada1c2b4e1badf95d28e231c7267d60
Matias <matiaslimina@gmail.com>
2025-09-03 18:50:17 -0300

    crear nombre,legajo y promedio

--- Profe Hualpa/Condicionales_hualpa/tarea2.py
+++ Profe Hualpa/Condicionales_hualpa/tarea2.py
@@ -1,4 +1,12 @@
 #Sistemas de calificación con promoción
+nombre = input("Ingrese su nombre completo").title()
+legajo = input("Ingrese su legajo")
+
 nota1 = int(input("Ingrese su primer nota "))
 nota2 = int(input("Ingrese su segunda nota "))
 nota3 = int(input("Ingrese su tercer nota "))
+
+promedio = (nota1+nota2+nota3)/3
+
+if nota1 < 4 or nota2 < 4 or nota3 < 4:
+    print(f"Alumno: {nombre} \n Legajo: {legajo} \n  ")
