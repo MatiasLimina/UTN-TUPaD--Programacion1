f932db4c6bc7b974348b274a5d54b87476a75c04
Matias <matiaslimina@gmail.com>
2025-09-03 08:58:44 -0300

    Principal ejercicio 8

--- Unidad 3/tp_repetitivos.py
+++ Unidad 3/tp_repetitivos.py
@@ -7,3 +7,9 @@ def es_par(n):
 def es_impar(n):
     return (n%2 != 0 )
 
+CANT_NUMEROS = 5
+lista_numeros = []
+def principal():
+    for i in range(1,CANT_NUMEROS):
+        lista_numeros[i] = input(int("Ingrese el número: ",i))
+        
