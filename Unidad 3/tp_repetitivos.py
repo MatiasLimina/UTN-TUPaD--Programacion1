7e87050cae806b1b9a2d0b4b3d7435ff4bda28e0
Matias <matiaslimina@gmail.com>
2025-09-04 17:14:34 -0300

    Resolución ejercicio 8

--- Unidad 3/tp_repetitivos.py
+++ Unidad 3/tp_repetitivos.py
@@ -7,13 +7,25 @@ def es_par(n):
 def es_impar(n):
     return (n%2 != 0 )
 import random
-CANT_NUMEROS = 5
+CANT_NUMEROS = 100
+lista_numeros = []
 def principal():
-    lista_numeros = []
+    cont_par = 0
+    cont_impar = 0
+    cont_positivo = 0
+    cont_negativo = 0
     for i in range(0,CANT_NUMEROS):
-        lista_numeros.insert(i,(random.randint(1,100)))
+        lista_numeros.insert(i,(random.randint(-100,100)))
         print(lista_numeros[i])
+        if es_par(lista_numeros[i]):
+            cont_par += 1
+        elif es_impar(lista_numeros[i]):
+            cont_impar +=1
         
-    
+        if lista_numeros[i]>0:
+            cont_positivo +=1
+        elif lista_numeros[i]<0:
+            cont_negativo +=1
+    print(f"Números positivos: {cont_positivo} \n Números negativos: {cont_negativo} \n Números pares: {cont_par} \n Números impares: {cont_impar}")
 
 principal()
