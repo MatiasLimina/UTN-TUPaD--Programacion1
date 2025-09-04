96be6d2afa98bfae7c01877dd10ade336de102fe
Matias <matiaslimina@gmail.com>
2025-09-04 10:54:06 -0300

    Creacion tarea4
    Añadir variables

---
+++ Profe Hualpa/Condicionales_hualpa/tarea4.py
@@ -0,0 +1,5 @@
+#Categoría de conductores
+nombre = input("Ingrese su nombre completo ").title()
+edad = int(input("Ingrese su edad "))
+exp = int(input ("Ingrese sus años de experiencia manejando"))
+categoria = ""
