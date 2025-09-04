5d8b2a0d80d4b07d7b25e9822e6b3b08589f9903
Matias <matiaslimina@gmail.com>
2025-09-04 11:13:54 -0300

    Validar cant de productos

--- Profe Hualpa/Condicionales_hualpa/tarea5.py
+++ Profe Hualpa/Condicionales_hualpa/tarea5.py
@@ -1,1 +1,13 @@
 #Simulador de carrito de compras
+nombre = input("Ingrese su nombre completo ").title()
+
+pos_prod = False
+
+while pos_prod == False:
+    productos = int(input("Ingrese la cantidad de productos comprados "))
+    if productos>0:
+        pos_prod = True
+    else:
+        print ("No puede ingresar un número negativo de productos comprados")
+
+
