232e2e274ea57b70f1e2f29310b68e008a9e2840
Matias <matiaslimina@gmail.com>
2025-09-04 11:03:55 -0300

    Sistema de clasificacion completo

--- Profe Hualpa/Condicionales_hualpa/tarea4.py
+++ Profe Hualpa/Condicionales_hualpa/tarea4.py
@@ -1,5 +1,17 @@
 #Categoría de conductores
 nombre = input("Ingrese su nombre completo ").title()
 edad = int(input("Ingrese su edad "))
-exp = int(input ("Ingrese sus años de experiencia manejando"))
+exp = float(input ("Ingrese sus años de experiencia manejando "))
 categoria = ""
+if edad < 18:
+    categoria = "No puede conducir"
+elif edad >= 18 and exp < 1:
+    categoria = "Principiante"
+elif edad >= 21 and (exp >= 1 and exp <= 5):
+    categoria = "Conductor intermedio"
+elif edad >= 30 and exp > 10:
+    categoria = "Conductor profesional"
+else:
+    categoria = "Conductor estándar"
+
+print(f"{nombre} \n {edad} años \n Experiencia de: {exp} años \n Clasificación: {categoria}")
