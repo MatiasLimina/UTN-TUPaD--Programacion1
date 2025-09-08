785a2e8fec13a3200b2ee7c7a2e80f2d4e02d225
Matias <matiaslimina@gmail.com>
2025-09-08 12:12:56 -0300

    pruebas

--- Trabajos en clase/Listas/Ejercicio_listas.py
+++ Trabajos en clase/Listas/Ejercicio_listas.py
@@ -3,6 +3,7 @@ filas = 5
 columnas = 5
 carton_bingo1 = [[0 for  _ in range (columnas)]for _ in range(filas)]
 numeros = random.sample(range(1,51), 25)
+numeros_sorteo = numeros
 print (numeros)
 for i in carton_bingo1:
     for j in i:
@@ -14,4 +15,15 @@ for i in range(filas):
         carton_bingo1[i][j] = numeros.pop() 
         print (carton_bingo1[i][j],end =" ")
     print()
+print(carton_bingo1)
+
+for x in numeros_sorteo:
+    for i in range(filas):
+        for j in range(columnas):
+            if carton_bingo1[i][j] == x:
+                carton_bingo1[i][j] = 0
+            else:
+                break        
+
+
 print(carton_bingo1)
