4733fbe084b08547ac76b7197a63d32f3dd8b351
Matias <matiaslimina@gmail.com>
2025-09-10 09:16:51 -0300

    crear batalla naval

---
+++ Profe Hualpa/Listas/batalla_naval.py
@@ -0,0 +1,16 @@
+
+"""
+2. Cruce de matrices (tablero naval)
+
+Crear dos matrices 5x5:
+
+Una representa un “océano” con barcos colocados aleatoriamente (con 1 = barco y 0 = agua).
+
+Otra representa los disparos de un jugador.
+
+Cada vez que el jugador dispara, se marca con una “X” si fue acierto o “O” si fue fallo.
+
+El juego termina cuando todos los barcos son hundidos.
+👉 Similar al Bingo, pero con coordenadas y validaciones.
+
+"""
