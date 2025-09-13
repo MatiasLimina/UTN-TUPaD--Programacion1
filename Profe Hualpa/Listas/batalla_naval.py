e3a5a01911d8b436f5924300025e35f8bc196829
Matias <matiaslimina@gmail.com>
2025-09-13 12:41:37 -0300

    Colocación de los barcos en el tablero

--- Profe Hualpa/Listas/batalla_naval.py
+++ Profe Hualpa/Listas/batalla_naval.py
@@ -21,8 +21,7 @@ barcos_hundidos_usuario = 0
 barcos_hundidos_pc = 0
 
 def crear_barcos(barcos_cont):
-    
-    if barcos_cont <= 5:
+    if barcos_cont < 5:
         pos = random.randint(0,1)
         return pos
     else:
@@ -31,19 +30,19 @@ def crear_barcos(barcos_cont):
 
 
 
-matriz_usuario = [["-" for  _ in range (MAX_BARCOS)]for _ in range(MAX_BARCOS)]
-matriz_pc = [["-" for  _ in range (MAX_BARCOS)]for _ in range(MAX_BARCOS)]
+matriz_usuario = [["0" for  _ in range (MAX_BARCOS)]for _ in range(MAX_BARCOS)]
+matriz_pc = [["0" for  _ in range (MAX_BARCOS)]for _ in range(MAX_BARCOS)]
 
 def imprimir_matriz_u (m_u):
-    for i in range (MAX_BARCOS):
-        for j in range(MAX_BARCOS):
+    for i in range (5):
+        for j in range(5):
             print(m_u[i][j],end = " ")
-    print()
+        print()
 def imprimir_matriz_pc(m_pc):
-    for i in range (MAX_BARCOS):
-        for j in range(MAX_BARCOS):
+    for i in range (5):
+        for j in range(5):
             print(m_pc[i][j],end = " ")
-    print()
+        print()
 
 for i in range(MAX_BARCOS):
     print(matriz_pc[i])
@@ -51,7 +50,27 @@ print("---------------")
 for i in range(MAX_BARCOS):
     print(matriz_usuario[i])
 
+imprimir_matriz_pc(matriz_pc)
+imprimir_matriz_u(matriz_usuario)
+contador = 0
+for i in range (5):
+        for j in range(5):
+            matriz_pc[i][j] = crear_barcos(contador)
+            if matriz_pc[i][j] == 1:
+                contador+=1
+contador = 0
+for i in range (5):
+        for j in range(5):
+            matriz_usuario[i][j] = crear_barcos(contador)
+            if matriz_usuario[i][j] == 1:
+                contador+=1
 
+
+for i in range(MAX_BARCOS):
+    print(matriz_pc[i])
+print("---------------")
+for i in range(MAX_BARCOS):
+    print(matriz_usuario[i])
 
 
 
