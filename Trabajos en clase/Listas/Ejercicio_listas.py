52e55864750caceae30ad4aa401a036420b8d879
Matias <matiaslimina@gmail.com>
2025-09-13 12:07:36 -0300

    Bingo terminado

--- Trabajos en clase/Listas/Ejercicio_listas.py
+++ Trabajos en clase/Listas/Ejercicio_listas.py
@@ -3,34 +3,33 @@ filas = 5
 columnas = 5
 carton_bingo1 = [[0 for  _ in range (columnas)]for _ in range(filas)]
 numeros = random.sample(range(1,51), 25)
-print (numeros)
-for i in carton_bingo1:
-    for j in i:
-        print(j,end=" ")
-    print()
 
-for i in range(filas):
-    for j in range(columnas):
-        carton_bingo1[i][j] = numeros.pop() 
-        print (carton_bingo1[i][j],end =" ")
-    print()
-print("----------------------")
+print ("Su carton de bingo es: ")
 
 for i in carton_bingo1:
     print(i)
 numeros_sorteo = []
 bingo = False
+fila_completa= []
 while not bingo :
     numero_sorteado = random.randint(1,50)
     if numero_sorteado not in numeros_sorteo:
         numeros_sorteo.append(numero_sorteado)
         print(f"Se sortea el {numero_sorteado}")
+    
     for i in range(filas):
         for j in range(columnas):
             if carton_bingo1[i][j] == numero_sorteado:
                 carton_bingo1[i][j] = 0
                 print ("Lo tenés")
                 break
+    #if fila_completa <= 5:
+    for indice, fila in enumerate(carton_bingo1):
+        if all(x == 0 for x in fila) and indice not in fila_completa:
+            fila_completa.append(indice)
+            print(f"Linea completa en la fila {indice+1}", carton_bingo1[indice])
+
+    
     
     bingo = all(
         carton_bingo1[i][j] == 0
@@ -44,4 +43,6 @@ while not bingo :
 
 
 
-print("Nuevo: ",carton_bingo1)
+print("Su carton completo: ")
+for i in carton_bingo1:
+    print(i)
