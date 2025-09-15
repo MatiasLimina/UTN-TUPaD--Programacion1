23366aca5534bd7ff3e0707f9309f758ef9b29cb
Matias <matiaslimina@gmail.com>
2025-09-15 11:56:26 -0300

    opc 5 funciona

--- Trabajos en clase/evaluaciones/Evaluacion_caso1.py
+++ Trabajos en clase/evaluaciones/Evaluacion_caso1.py
@@ -1,17 +1,19 @@
-titulos = ["El señor de los anillos", "Orgullo y prejuicio","La divina comedia"]
-ejemplares = [3,5,9]
+titulos = ["El señor de los anillos", "Orgullo y prejuicio","La divina comedia","Pepe aventuras"]
+ejemplares = [3,5,9,0]
 salir = False
 
 while not salir:
     opcion = input("Que desea hacer: 1) Ingresar nuevos titulos al caralogo \n 2) Ingresar el numero de copias disponibles de un titulo \n 3) Mostrar el catálogo con copias disponibles \n 4) Buscar la disponibilidad de un titulo especifico \n 5) Mostrar ejemplares que sin copias disponibles \n 6) Agregar un titulo nuevo y sus copias disponibles \n 7) Actualizar ejemplares (prestamo/devolución) \n 8) Ver catálogo ")
     while not opcion.isnumeric():
         opcion = input("Que desea hacer: 1) Ingresar nuevos titulos al caralogo \n 2) Ingresar el numero de copias disponibles de un titulo \n 3) Mostrar el catálogo con copias disponibles \n 4) Buscar la disponibilidad de un titulo especifico \n 5) Mostrar ejemplares que sin copias disponibles \n 6) Agregar un titulo nuevo y sus copias disponibles \n 7) Actualizar ejemplares (prestamo/devolución) \n 8) Ver catálogo ")
+    #Ingreso de nuevos titulos al catalogo general
     if opcion == "1":
         nuevo_titulo = input("Ingrese un nuevo titulo para agregar a la biblioteca ").lower().capitalize()
         titulos.append(nuevo_titulo.title())
         ejemplares.append(0)
         for i in range(len(titulos)):
             print(titulos[i],":",ejemplares[i])
+    #Ingrese de numero de copias discponibles a un titulo en especifico
     elif opcion == "2":
         buscar_titulo = input("Ingrese el nombre del titulo al cual quiere actualizar su numero de copias ").capitalize()
         while buscar_titulo not in titulos:
@@ -23,14 +25,27 @@ while not salir:
         if buscar_titulo in titulos:
             indice_tit_buscado = titulos.index(buscar_titulo)
             ejemplares[indice_tit_buscado] = input(f"Ingrese la cantidad de ejemplares del libro {titulos[indice_tit_buscado] }")
+    #Catálogo de titulos con copias disponibles
     elif opcion == "3":
         catalogo_con_copias = []
-        for 
-        
-    
-    
-    
-    
+        for i in range (len(titulos)):
+            if ejemplares[i] > 0:
+                catalogo_con_copias.append(titulos[i])
+        print(f"Los titulos con copias disponibles son: {catalogo_con_copias}")
+    #Buscar la cantidad de ejemplares de un titulo puntual
+    elif opcion == "4":
+        disp_titulo = input("Ingrese el titulo del que desea saber la cantidad de copias disponibles ").lower().capitalize()
+        while disp_titulo not in titulos:
+            print("El titulo ingresado no esta en el catálogo...")
+            salir_pregunta = input("Si quiere salir ingrese s Si quiere continuar presione cualquier otra tecla ").lower()
+            if salir_pregunta == "s":
+                break
+            disp_titulo = input("Ingrese el titulo del que desea saber la cantidad de copias disponibles ").lower().capitalize()
+        if disp_titulo in titulos:
+            indice_tit_buscado = titulos.index(disp_titulo)
+            print(f"El titulo {titulos[indice_tit_buscado]} tiene una cantidad de {ejemplares[indice_tit_buscado]} copias")
     
     break
 print("Fuera del ciclo")
+
+
