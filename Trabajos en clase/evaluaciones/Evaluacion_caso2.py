04939662cff0aa57ba67822d2acfa44b45533c07
Matias <matiaslimina@gmail.com>
2025-09-17 08:20:40 -0300

    Caso 2 Mostrar menu

---
+++ Trabajos en clase/evaluaciones/Evaluacion_caso2.py
@@ -0,0 +1,9 @@
+especialidades = ["Cardiología", "Dermatología", "Pediatría"]
+cupos = [5, 3, 10]
+
+salir = False
+while not salir:
+
+    opcion = input("Ingrese que desea hacer: 1)Ingresar especialidad \n 2)Ingresar cupos disponibles para una especialidad específica \n 3)Mostrar agenda \n 4)Consultar los cupos disponibles en una especialidad específica \n 5)Mostrar especialidades sin cupos disponibles \n 6) Agregar nueva especialidad y el numero de cupos disponibles para la misma \n 7)Ver las especialidades sin cupos disponibles \n 8)Actualizar cupos (Reservar/Cancelar) \n 9)Ver agenda completa \n 10)Salir \n ")
+    
+    break
