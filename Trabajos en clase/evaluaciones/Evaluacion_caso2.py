36719a53ca96a91181af43083e73cdd3aecc6f26
Matias <matiaslimina@gmail.com>
2025-09-19 16:42:51 -0300

    opc 1/4/5
    Modificacion de la opcion 1, y completada las opciones 4 y 5

--- Trabajos en clase/evaluaciones/Evaluacion_caso2.py
+++ Trabajos en clase/evaluaciones/Evaluacion_caso2.py
@@ -1,22 +1,24 @@
-especialidades = ["Cardiología", "Dermatología", "Pediatría"]
-cupos = [5, 3, 10]
+especialidades = ["Cardiología", "Dermatología", "Pediatría","Psicología"]
+cupos = [5, 3, 10,0]
 
 salir = False
 while not salir:
-    opcion = input("Ingrese que desea hacer: 1)Ingresar especialidad \n 2)Ingresar cupos disponibles para una especialidad específica \n 3)Mostrar agenda \n 4)Consultar los cupos disponibles en una especialidad específica \n 5)Mostrar especialidades sin cupos disponibles \n 6) Agregar nueva especialidad y el numero de cupos disponibles para la misma \n 7)Ver las especialidades sin cupos disponibles \n 8)Actualizar cupos (Reservar/Cancelar) \n 9)Ver agenda completa \n 10)Salir \n ")
+    opcion = input("Ingrese que desea hacer:\n 1)Ingresar especialidad \n 2)Ingresar cupos disponibles para una especialidad específica \n 3)Mostrar agenda \n 4)Consultar los cupos disponibles en una especialidad específica \n 5)Mostrar especialidades sin cupos disponibles \n 6) Agregar nueva especialidad y el numero de cupos disponibles para la misma \n 7)Ver las especialidades sin cupos disponibles \n 8)Actualizar cupos (Reservar/Cancelar) \n 9)Ver agenda completa \n 10)Salir \n ")
     while not opcion.isnumeric():
-        opcion = input("Ingrese que desea hacer: 1)Ingresar especialidad \n 2)Ingresar cupos disponibles para una especialidad específica \n 3)Mostrar agenda \n 4)Consultar los cupos disponibles en una especialidad específica \n 5)Mostrar especialidades sin cupos disponibles \n 6) Agregar nueva especialidad y el numero de cupos disponibles para la misma \n 7)Ver las especialidades sin cupos disponibles \n 8)Actualizar cupos (Reservar/Cancelar) \n 9)Ver agenda completa \n 10)Salir \n ")
+        opcion = input("Ingrese que desea hacer:\n 1)Ingresar especialidad \n 2)Ingresar cupos disponibles para una especialidad específica \n 3)Mostrar agenda \n 4)Consultar los cupos disponibles en una especialidad específica \n 5)Mostrar especialidades sin cupos disponibles \n 6) Agregar nueva especialidad y el numero de cupos disponibles para la misma \n 7)Ver las especialidades sin cupos disponibles \n 8)Actualizar cupos (Reservar/Cancelar) \n 9)Ver agenda completa \n 10)Salir \n ")
     #Ingreso de nueva especialidad
     if opcion == "1":
-        nueva_espe = input("Ingrese una nueva especialidad ").lower().capitalize()
-        while nueva_espe in especialidades:
-            print("La especialidad ingreada ya esta dentro del sistema")
+        cant_nuevas_espe = int(input("Cuantas especialidades desea agregar? "))
+        for j in range(cant_nuevas_espe):
             nueva_espe = input("Ingrese una nueva especialidad ").lower().capitalize()
-        if nueva_espe not in especialidades:
-            especialidades.append(nueva_espe)
-            cupos.append(0)
-        for i in range(len(cupos)):
-            print (f"Especialidad: {especialidades[i]} Cupos: {cupos[i]} ")
+            while nueva_espe in especialidades:
+                print("La especialidad ingreada ya esta dentro del sistema")
+                nueva_espe = input("Ingrese una nueva especialidad ").lower().capitalize()
+            if nueva_espe not in especialidades:
+                especialidades.append(nueva_espe)
+                cupos.append(0)
+            for i in range(len(cupos)):
+                print (f"Especialidad: {especialidades[i]} Cupos: {cupos[i]} ")
     #Ingresar cupos disponibles por especialidad
     if opcion == "2":
         for i in range(len(especialidades)):
@@ -30,6 +32,28 @@ while not salir:
         print("Le mostraremos nuestra agenda")
         for i in range(len(especialidades)):
             print(f"Especialidad: {especialidades[i]} Cupos: {cupos[i]}")
+    #Consultar cupos especialidad especifica
+    if opcion == "4":
+        cupos_espe = input("De que especialidad desea chequear disponibilidad ").lower().capitalize()
+        while cupos_espe not in especialidades:
+            print("No contamos con esa especialidad en este establecimiento")
+            print("Le mostraremos nuestras especialidades a continuacion")
+            for i in range(len(especialidades)):
+                print(especialidades[i])
+            cupos_espe = input("Ingrese una de las especialidades mostradas anteriormente ").lower().capitalize()
+        if cupos_espe in especialidades:
+            index = especialidades.index(cupos_espe)
+            print(f"La cantidad de cupos disponibles para {cupos_espe} es de {cupos[index]}")
+    #Mostrar todas las especialidades sin cupos disponibles
+    if opcion == "5":
+        espe_sin_cupos=[]
+        for i in range(len(especialidades)):
+            if cupos[i] == 0:
+                espe_sin_cupos.append(especialidades[i])
+        print("Las especialidades sin cupo disponibles son: ")
+        for i in espe_sin_cupos:
+            print(i)
+    
     
     #Salir del menu
     if opcion == "10":
