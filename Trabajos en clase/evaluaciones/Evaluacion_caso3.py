5ac0023c5dd6c49673f5ee5150e5903f1cf8606c
Matias <matiaslimina@gmail.com>
2025-09-21 12:37:14 -0300

    Mostrar menu

--- Trabajos en clase/evaluaciones/Evaluacion_caso3.py
+++ Trabajos en clase/evaluaciones/Evaluacion_caso3.py
@@ -1,2 +1,16 @@
 tarjetas = ["1234567890123456", "9876543210987654", "5555666677778888"]
 saldos = [150.50, 25.00, -10.00]
+
+salir = False
+while not salir:
+    print ("MENU")
+    print("1) Ingresar nuevas tarjetas al sistema")
+    print("2) Asignar saldo para tarjetas recientemente agregadas")
+    print("3) Ver tarjetas registradas y sus saldos")
+    print("4) Ver salfo de una tarjeta específica")
+    print("5) Ver listado de tarjetas con saldo negativo o cero")
+    print("6) Registrar una nueva tarjeta y su saldo actual")
+    print("7) Aumentar o discminuir el saldo de una tarjeta")
+    print("8) Salir")
+    opc = input("Eliga una opción: ")
+    
