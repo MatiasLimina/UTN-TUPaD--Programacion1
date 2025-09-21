569658c1c7d6791d68e6711192c3bf3a7a792d69
Matias <matiaslimina@gmail.com>
2025-09-21 13:02:34 -0300

    Arreglo opc1 y opc 2 completada
    Problema de logica en la opc 1 solucionado, mal puestas las condiciones

--- Trabajos en clase/evaluaciones/Evaluacion_caso3.py
+++ Trabajos en clase/evaluaciones/Evaluacion_caso3.py
@@ -16,20 +16,27 @@ while not salir:
     while not opc.isnumeric():
         opc = input("Eliga una opción válida: ")
     
-    if opc == "1":
+    if opc == "1": #Ingreso de nuevas tarjetas al sistema
         num_nuevas_tarj = int(input("Cuántas tarjetas desea registrar? Máximo de 5 tarjetas a la vez "))
-        if num_nuevas_tarj >5:
+        if num_nuevas_tarj >5: #Comprueba que el valor ingresado no exceda el limite
             print("El número ingresado excede el limite del sistema")
         else:
             for i in range(num_nuevas_tarj):
                 nueva_tarjeta = input("Ingrese los datos de la nueva tarjeta: (Debe ser un numero de 16 dígitos)")
-                while not nueva_tarjeta.isnumeric() or len(nueva_tarjeta)>16 or len(nueva_tarjeta)<16:
-                    print ("Ingreso mal los datos de la nueva tarjeta")
+                #El bucle se ejecuta cuando el formato de la tarjeta a registrar no es el correcto o cuando la tarjeta en cuestion ya esta en el sistema
+                while not nueva_tarjeta.isnumeric() or len(nueva_tarjeta)>16 or len(nueva_tarjeta)<16 or nueva_tarjeta in tarjetas:
+                    print ("Ingreso mal los datos de la nueva tarjeta o ya se encuentra en el sistema")
                     nueva_tarjeta = input("Ingrese los datos de la nueva tarjeta: (Debe ser un numero de 16 dígitos)")
-                if len(nueva_tarjeta) == 16:
+                if len(nueva_tarjeta) == 16: #Si no hay porblemas la tarjeta se agrega al sist
                     tarjetas.append(nueva_tarjeta)
                     saldos.append(0)
-        print(tarjetas,saldos)
+    elif opc == "2":#Asignar saldo para tarjetas recientemente agregadas
+        if nueva_tarjeta == 0: #Si no se agregaron tarjetas nuevas no permite colocar saldos nuevos
+            print("No hay nuevas tarjetas registradas")
+        else:
+            for i in range(num_nuevas_tarj): #Permite al usuario igresar los saldos en orden
+                nuevo_sueldo = float(input("Ingrese el sueldo de las nuevas tarjetas en orden"))
+                saldos.append(nuevo_sueldo)
     elif opc == "8":
         print("Desea salir?")
         aux_salir = input("Si / No ").upper()
