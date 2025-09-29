0e80ab4da078e9eea69fd5b06ca6bf23b223a703
Matias <matiaslimina@gmail.com>
2025-09-29 11:41:00 -0300

    Crear funcion array palabra
    Funcion que me crea un array con la palabra para poder comparar cada letra con el input del usuario posteriormente

--- Trabajos en clase/Ejercicio_funciones/ahorcado.py
+++ Trabajos en clase/Ejercicio_funciones/ahorcado.py
@@ -11,10 +11,34 @@ def ocultar_palabra(palabra_ocul):
         oculta = oculta + "_"
     return oculta
 
+def array_palabra(palabra:str,palabra_oculta):
+    aux_palabra = []
+    aux_oculta= []
+    for i in range(len(palabra)):
+        aux_palabra.append[palabra[i]]
+        aux_oculta.append[palabra_oculta[i]]
 
 
+def mostrar_ahorcado(oculta):
+    print(f"{oculta} / Quedan x intentos")
+
+#main
 palabras = ["pera", "manzana","banana","naranja"]
 palabra_ahorcado = elegir_palabra(palabras)
 oculta = ocultar_palabra(palabra_ahorcado)
+intentos = 0
 print (palabra_ahorcado)
 print(oculta)
+
+salir = False
+while not salir:
+    turno = intento_letra(palabra_ahorcado,oculta)
+    if turno == False:
+        intentos += 1
+    else:
+        oculta=turno
+        mostrar_ahorcado(oculta)
+    if intentos == 6:
+        print("Intentos maximos alcanzados \n Perdiste!")
+        salir == True
+
