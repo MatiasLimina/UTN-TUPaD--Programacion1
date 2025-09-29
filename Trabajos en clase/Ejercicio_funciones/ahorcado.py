c82b7e43a4da98e3fafcfda280b23b01a2ab3194
Matias <matiaslimina@gmail.com>
2025-09-29 11:13:02 -0300

    Crear funcion oculta palabra

--- Trabajos en clase/Ejercicio_funciones/ahorcado.py
+++ Trabajos en clase/Ejercicio_funciones/ahorcado.py
@@ -4,9 +4,17 @@ def elegir_palabra (palabras): #Selecciona una palabra random para jugar
     num_random = random.randrange(0,len(palabras))
     return palabras[num_random]
 
-
+def palabra_oculta(palabra_ocul):
+    len_oculta = len(palabra_ocul)
+    oculta = ""
+    for i in range(len_oculta):
+        oculta = oculta + "_"
+    return oculta
 
 
 
 palabras = ["pera", "manzana","banana","naranja"]
 palabra_ahorcado = elegir_palabra(palabras)
+oculta = palabra_oculta(palabra_ahorcado)
+print (palabra_ahorcado)
+print(oculta)
