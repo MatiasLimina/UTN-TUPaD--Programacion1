9c58c2a57aa31275a8e3009f42b074ddd32ed32a
Matias <matiaslimina@gmail.com>
2025-10-01 12:52:09 -0300

    Resolucion ejercicio 3

--- Unidad 5/TP_funciones.py
+++ Unidad 5/TP_funciones.py
@@ -1,27 +1,19 @@
 def hola_mundo():
-    print("Hola Mundo!")
+    return print("Hola Mundo!")
 
 def saludar_usuario(nombre):
-    print(f"Hola {nombre}!")
+    return print(f"Hola {nombre}!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def informacion_personal(nombre,apellido,edad,residencia):
+    return print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
 
 
 #Main
+nombre = input("Ingrese su nombre ").capitalize()
+apellido = input("Ingrese su apellido ").capitalize()
+edad = int(input("Ingrese su edad "))
+residencia = input("Ingrese su lugar de residencia ").capitalize()
 hola_mundo()
-saludar_usuario(input("Ingrese su nombre ").capitalize())
+saludar_usuario(nombre)
+informacion_personal(nombre,apellido,edad,residencia)
