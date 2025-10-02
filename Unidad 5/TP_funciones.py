d6318fc1d09d52e1e02875c2681d709a9dc65075
Matias <matiaslimina@gmail.com>
2025-10-02 20:25:43 -0300

    Resolucion ejercicio 7

--- Unidad 5/TP_funciones.py
+++ Unidad 5/TP_funciones.py
@@ -22,6 +22,14 @@ def tabla_multiplicar(numero):
     print(f"Tabla de multiplicar de {numero}")
     for i in range(1,11):
         print(f"{i} x {numero} = {i*numero}")
+
+def operaciones_basicas(a,b):
+    suma = a+b
+    resta = a-b
+    multiplicacion = a*b
+    division = a/b
+    operaciones = (suma,resta,multiplicacion,division)
+    return operaciones
 #Main
 PI = math.pi
 nombre = input("Ingrese su nombre ").capitalize()
@@ -30,11 +38,18 @@ edad = int(input("Ingrese su edad "))
 residencia = input("Ingrese su lugar de residencia ").capitalize()
 radio = float(input("Ingrese el radio de un circulo"))
 segundos = int(input("Ingrese una cantidad de segundos: "))
-num = int(input("Ingrese un numero del que desea saber su tabla de multiplicar hasta el 10 "))
+num_multiplicar = int(input("Ingrese un numero del que desea saber su tabla de multiplicar hasta el 10 "))
+num1 = int(input("Ingrese el numero 1 "))
+num2 = int(input("Ingresar el num 2 "))
 hola_mundo()
 saludar_usuario(nombre)
 informacion_personal(nombre,apellido,edad,residencia)
 print("Area del circulo: ",calcular_area_circulo(radio))
 print("Perimetro del circulo: ",calcular_perimetro_circulo(radio))
 segundos_a_horas(segundos)
-tabla_multiplicar(num)
+tabla_multiplicar(num_multiplicar)
+ope_basicas = operaciones_basicas(num1,num2)
+aux_op_basicas = ["Suma: ","Resta: ","Multiplicación: ","División: "]
+print(f"Las operacion básicas entre {num1} y {num2} son: ")
+for i in range(len(ope_basicas)):
+    print(f"{aux_op_basicas[i]}{ope_basicas[i]}")
