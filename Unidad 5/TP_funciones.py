9d1dd67fb49a294a1c542c178d6bae3b2476caf9
Matias <matiaslimina@gmail.com>
2025-10-02 19:56:02 -0300

    Mostras el area y el perimetro del circulo

--- Unidad 5/TP_funciones.py
+++ Unidad 5/TP_funciones.py
@@ -9,7 +9,7 @@ def informacion_personal(nombre,apellido,edad,residencia):
     return print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
 def calcular_area_circulo(radio):
-    return PI*(radio**2)
+    return PI*(radio ** 2)
 
 def calcular_perimetro_circulo(radio):
     return 2*PI*radio
@@ -20,6 +20,9 @@ nombre = input("Ingrese su nombre ").capitalize()
 apellido = input("Ingrese su apellido ").capitalize()
 edad = int(input("Ingrese su edad "))
 residencia = input("Ingrese su lugar de residencia ").capitalize()
+radio = float(input("Ingrese el radio de un circulo"))
 hola_mundo()
 saludar_usuario(nombre)
 informacion_personal(nombre,apellido,edad,residencia)
+print("Area del circulo: ",calcular_area_circulo(radio))
+print("Perimetro del circulo: ",calcular_perimetro_circulo(radio))
