38a149a77d940413085759bacf9600167267abe4
Matias <matiaslimina@gmail.com>
2025-10-11 17:06:54 -0300

    Resolución E_4

--- Unidad 6/TP_estructuraDeDatosComplejas.py
+++ Unidad 6/TP_estructuraDeDatosComplejas.py
@@ -14,3 +14,24 @@ print(precios_frutas)
 #Ejercicio 3
 frutas = list(precios_frutas.keys())
 print(frutas)
+
+#Ejercicio 4
+def almacenar_contactos():
+    dic_contacts = dict()
+    for i in range(2):
+        dic_contacts[input("Coloque el nombre del contacto ").capitalize()] = int(input("Coloque el numero de telefono "))
+    return dic_contacts
+def buscar_numero(contactos,nombre):
+    num_buscado = contactos.get(nombre)
+    return num_buscado
+
+
+contactos = almacenar_contactos()
+print(contactos)
+nombre = input("Ingrese el nombre de quien desea saber su numero ").capitalize()
+solicitar_num = buscar_numero(contactos,nombre)
+print (solicitar_num)
+if solicitar_num == None:
+    print("Contacto no encontrado")
+else:
+    print(f"El numero de telefono de {nombre} es {solicitar_num}")
