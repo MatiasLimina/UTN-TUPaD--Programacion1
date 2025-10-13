b5247632dd959d4b401396c6e056f079d520d6d0
Matias <matiaslimina@gmail.com>
2025-10-13 11:44:45 -0300

    Crear funcion menu/opcion

--- Trabajos en clase/Manejo_archivos/Registro_productos.py
+++ Trabajos en clase/Manejo_archivos/Registro_productos.py
@@ -3,17 +3,33 @@ productos = [
     {"nombre":"papa","precio":15},
     {"nombre":"tomate","precio":10}
 ]
-try:
-    with open("Productos.csv","w",newline="") as archivo:
-        campos = ["nombre","precio"]
-        escritor = csv.DictWriter(archivo,fieldnames=campos)
-        escritor.writeheader()
-        escritor.writerows(productos)
-    with open("Productos.csv","r") as archivo:
-        lector = csv.DictReader(archivo)
-        for fila in lector:
-            print(fila)
-        
-except FileExistsError:
-    print("El archivo ya existe")
+def abrir_archivo():
+    try:
+        with open("Productos.csv","w",newline="") as archivo:
+            campos = ["nombre","precio"]
+            escritor = csv.DictWriter(archivo,fieldnames=campos)
+            escritor.writeheader()
+            escritor.writerows(productos)
+        with open("Productos.csv","r") as archivo:
+            lector = csv.DictReader(archivo)
+            for fila in lector:
+                print(fila)
+            
+    except FileExistsError:
+        print("El archivo ya existe")
 
+# def menu(opc):
+#     match opc:
+#         case "1":
+            
+#         case "2":
+            
+#         case "3":
+            
+
+def elegir_menu():
+    print("====== MENU =====")
+    print("1) Mostrar productos registrados \n 2) Agregar nuevo producto \n 3) Eliminar producto \n 4) Salir ")
+    print("================")
+    opcion = input("Eliga una opción... ")
+    return opcion
