21ced0f1f0066ce3c778f17f32dcdcadfc5921ac
Matias <matiaslimina@gmail.com>
2025-10-15 16:20:21 -0300

    Resolucion E_1

--- Unidad 7/TP_archivos.py
+++ Unidad 7/TP_archivos.py
@@ -1,1 +1,13 @@
 #Ejercicio 1
+productos = [
+    "Banana,120.2,2\n",
+    "Manzana,100.0,4\n",
+    "Pera,200.4,7\n"
+    ] 
+RUTA_ARCHIVO = "Programacion 1/Universidad/Archivos/Productos.txt"
+
+with open(RUTA_ARCHIVO,"a") as archivo:
+    archivo.writelines(productos)
+
+#
+
