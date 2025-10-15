0aa7b957787d56beccfe1d490447bfc0f3abc1f2
Matias <matiaslimina@gmail.com>
2025-10-15 09:05:11 -0300

    .

--- Trabajos en clase/Manejo_archivos/Registro_productos.py
+++ Trabajos en clase/Manejo_archivos/Registro_productos.py
@@ -1,5 +1,5 @@
 import csv
-#armar una lista vacia, usar el realine y pasar cada linea como diccionario "a mano", no usar la libreria csv
+#armar una lista vacia, usar el realine y pasar cada linea como diccionario "a mano", no usar la libreria csv,prestar atencion a la decodificacion en la lectura
 def crear_archivo(): #Abre el archivo
     try:
         with open("Productos.csv","w",newline="") as archivo:
