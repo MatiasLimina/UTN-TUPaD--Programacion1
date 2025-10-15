b2cf380e294f7b6bd99a44a73c08ac14dde0d210
Matias <matiaslimina@gmail.com>
2025-10-15 16:32:48 -0300

    Resolucion E_2

--- Unidad 7/TP_archivos.py
+++ Unidad 7/TP_archivos.py
@@ -5,9 +5,17 @@ productos = [
     "Pera,200.4,7\n"
     ] 
 RUTA_ARCHIVO = "Programacion 1/Universidad/Archivos/Productos.txt"
+def crear_archivo():
+    with open(RUTA_ARCHIVO,"w") as archivo:
+        archivo.writelines(productos)
+archiv_productos = crear_archivo()
+#Ejercicio 2
 
-with open(RUTA_ARCHIVO,"a") as archivo:
-    archivo.writelines(productos)
+def leer_archivo():
+    with open(RUTA_ARCHIVO,"r") as archivo:
+        lineas = archivo.readlines()
+        for linea in lineas :
+            prod,precio,cant = linea.strip().split(",")
+            print(f"Producto: {prod}| Precio: ${precio}| Cantidad: {cant}")
 
-#
-
+leer_archivo()
