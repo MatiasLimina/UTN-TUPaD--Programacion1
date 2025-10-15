ad3567651f6d59595682cccdf031724453b57125
Matias <matiaslimina@gmail.com>
2025-10-15 19:07:46 -0300

    Resolucion E_4 Resolucion de este antes que E_3 ejercicio para implementar una solucion conjunsta a las demas consignas

--- Unidad 7/TP_archivos.py
+++ Unidad 7/TP_archivos.py
@@ -1,8 +1,8 @@
 #Ejercicio 1
 productos = [
-    "Banana,120.2,2\n",
-    "Manzana,100.0,4\n",
-    "Pera,200.4,7\n"
+    "Banana,120.2,2 \n",
+    "Manzana,100.0,4 \n",
+    "Pera,200.4,7 \n"
     ] 
 RUTA_ARCHIVO = "Programacion 1/Universidad/Archivos/Productos.txt"
 def crear_archivo():
@@ -12,14 +12,26 @@ def crear_archivo():
             print("Archivo 'Productos.txt' creado por primera vez.")
     except FileExistsError:
         print("El archivo 'Productos.txt' ya existe. No se sobreescribe.")
-archiv_productos = crear_archivo()
+crear_archivo()
 #Ejercicio 2
-
 def leer_archivo():
     with open(RUTA_ARCHIVO,"r") as archivo:
         lineas = archivo.readlines()
         for linea in lineas :
             prod,precio,cant = linea.strip().split(",")
             print(f"Producto: {prod}| Precio: ${precio}| Cantidad: {cant}")
+        return lineas
 
-leer_archivo()
+def crear_diccionario(lista:type=list):
+    lista_productos = []
+    for i in range(len(lista)):
+        producto, precio, cantidad = lista[i].strip().split(",")
+        lista_productos.append({"Productos": producto, "Precio": precio, "Cantidad": cantidad})
+    return lista_productos
+#Ejercicio 3
+aux_productos = leer_archivo()
+print (aux_productos)
+
+#Ejercicio 4
+lista_diccionarios_productos = crear_diccionario(aux_productos)
+print(lista_diccionarios_productos)
