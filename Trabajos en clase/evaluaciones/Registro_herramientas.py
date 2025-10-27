759b9680b150904fc6bf3f3c6733fecbf6d8772c
Matias <matiaslimina@gmail.com>
2025-10-27 10:40:16 -0300

    Crear Menu principal

---
+++ Trabajos en clase/evaluaciones/Registro_herramientas.py
@@ -0,0 +1,27 @@
+def main():
+    salir = False
+    while not salir:
+        opc = input("Ingrese una opción:\n 1) Cargar herramienta\n 2) Mostrar herramientas registradas\n 3) Modificar herramienta(Stock/Precio)\n 4) Elimina herramienta\n 5) Consultar disponibilidad\n 6) Listar productos sin stock\n 7) Salir\n Eliga una opción... ")
+        match opc:
+            case "1":
+                #Cargar herramientas
+                continue
+            case "2":
+                #Mostrar herramientas
+                continue
+            case "3":
+                #Modificar herramientas
+                continue
+            case "4":
+                #Elimina herramienta
+                continue
+            case "5":
+                #Consultar disponibilidad
+                continue
+            case "6":
+                #Listar producto sin stock
+                continue
+            case "7":
+                salir = True
+if __name__ == "__main__":
+    main()
